13a2a243b2bf0dbd1997d8aa81b0f581facab7e2
Mo-101 <akiniobong10@gmail.com>
2025-03-09 04:55:04 +0300

    up

--- Frontend/src/components/Progress.tsx
+++ Frontend/src/components/Progress.tsx
@@ -17,7 +17,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 transition-all bg-[#0af0ff]", indicatorColor)}
+      className={cn("h-full w-full flex-1 transition-all", indicatorColor || "bg-[#0af0ff]")}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
@@ -25,4 +25,3 @@ const Progress = React.forwardRef<
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
-
